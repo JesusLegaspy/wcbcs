d4835c94dfea7b5f60ebd99975ddd678ca5de173
Jesus Legaspy <jesus.legaspy@gmail.com>
2023-08-17 12:00:32 -0500

    Initial commit with spring initializr

---
+++ src/main/java/dev/legaspy/wcbcs/WcbcsApplication.java
@@ -0,0 +1,13 @@
+package dev.legaspy.wcbcs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WcbcsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WcbcsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/legaspy/wcbcs/WcbcsApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.legaspy.wcbcs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WcbcsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
